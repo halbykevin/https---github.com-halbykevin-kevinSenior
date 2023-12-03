9523106d1252569deb4147344eb541ad06d3fa97
OmarEl-Jundi <omar.eljundi@hotmail.com>
2023-12-03 11:42:42 +0200

    fixed the dark-light on all pages

--- script.js
+++ script.js
@@ -1,69 +1,31 @@
-document.getElementById("search-button").addEventListener("click", function () {
-  // Get the value of the search bar
-  var query = document.getElementById("search-bar").value.toLowerCase();
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("search-button")
+    .addEventListener("click", function () {
+      // Get the value of the search bar
+      var query = document.getElementById("search-bar").value.toLowerCase();
 
-  // Select all product cards
-  var productCards = document.querySelectorAll(".product-card");
+      // Select all product cards
+      var productCards = document.querySelectorAll(".product-card");
 
-  // Iterate over each product card
-  productCards.forEach(function (card) {
-    // Get the product name from the h3 element
-    var productName = card.querySelector("h3").textContent.toLowerCase();
+      // Iterate over each product card
+      productCards.forEach(function (card) {
+        // Get the product name from the h3 element
+        var productName = card.querySelector("h3").textContent.toLowerCase();
 
-    // Check if the product name includes the search query
-    if (productName.includes(query)) {
-      // If it does, display the product card
-      card.style.display = "block";
-    } else {
-      // Otherwise, hide the product card
-      card.style.display = "none";
-    }
-  });
+        // Check if the product name includes the search query
+        if (productName.includes(query)) {
+          // If it does, display the product card
+          card.style.display = "block";
+        } else {
+          // Otherwise, hide the product card
+          card.style.display = "none";
+        }
+      });
+    });
 });
 
 // dark-light mode
-document.addEventListener("DOMContentLoaded", function () {
-  // Set dark theme colors as default on page load
-  document.documentElement.style.setProperty("--primary-bg-color", "#171a21");
-  document.documentElement.style.setProperty("--secondary-bg-color", "#1b2838");
-  document.documentElement.style.setProperty("--text-color", "#ffffff");
-
-  const toggleSwitch = document.getElementById("darkmode-toggle");
-  toggleSwitch.addEventListener("change", switchTheme, false);
-
-  function switchTheme(e) {
-    if (e.target.checked) {
-      // Apply light theme when moon is showing
-      document.documentElement.style.setProperty(
-        "--primary-bg-color",
-        "#2a475e"
-      );
-      document.documentElement.style.setProperty(
-        "--secondary-bg-color",
-        "#66c0f4"
-      );
-      document.documentElement.style.setProperty(
-        "--primary-text-color",
-        "#ffffff"
-      );
-    } else {
-      // Apply dark theme when sun is showing
-      document.documentElement.style.setProperty(
-        "--primary-bg-color",
-        "#171a21"
-      );
-      document.documentElement.style.setProperty(
-        "--secondary-bg-color",
-        "#1b2838"
-      );
-      document.documentElement.style.setProperty(
-        "--primary-text-color",
-        "#000000"
-      );
-    }
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const toggleSwitch = document.getElementById("darkmode-toggle");
 
@@ -100,14 +62,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // fix dark mode button while scrolling
-window.addEventListener("scroll", function () {
-  var darkModeToggle = document.querySelector(".darkThemeBtn");
-  var scrollY = window.scrollY || window.pageYOffset;
+// window.addEventListener("scroll", function () {
+//   var darkModeToggle = document.querySelector(".darkThemeBtn");
+//   var scrollY = window.scrollY || window.pageYOffset;
 
-  if (scrollY > 100) {
-    // Adjust this value based on when you want the toggle to fix
-    darkModeToggle.style.position = "fixed";
-  } else {
-    darkModeToggle.style.position = "absolute";
-  }
-});
+//   if (scrollY > 100) {
+//     // Adjust this value based on when you want the toggle to fix
+//     darkModeToggle.style.position = "fixed";
+//   } else {
+//     darkModeToggle.style.position = "absolute";
+//   }
+// });
